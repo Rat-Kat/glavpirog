15cfdc55b83b617f7101fc8066d9e9be217d348b
Rat-Kat <kratush@gmail.com>
2016-06-23 00:32:13 +0300

    modal popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 $('.parallax').height($(window).height());
 
-
 $('.number-7').animateNumber({ number: 7}, 1500);
 $('.number-2000').animateNumber({number: 2000},1500);
 $('.number-2457').animateNumber({number: 2457},1500);
-
 
 $(function () {
     $('.second-page-order-form-date').datetimepicker(
@@ -13,4 +11,5 @@ $(function () {
     $('.second-page-order-form-time').datetimepicker(
         {pickDate: false, language: 'ru'}
     );
+    $('.second-page-address-subway').styler();
 });
