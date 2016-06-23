31b6abe23052de7b9da9c71837d9e0b1b60cf53a
Rat-Kat <kratush@gmail.com>
2016-06-24 01:23:03 +0300

    carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-$('.parallax').height($(window).height());
+$(document).ready(function(){
+    $('.parallax').height($(window).height());
 
-$('.number-7').animateNumber({ number: 7}, 1500);
-$('.number-2000').animateNumber({number: 2000},1500);
-$('.number-2457').animateNumber({number: 2457},1500);
+    $(".first-page-clients-gallery").jCarouselLite({
+        btnNext: ".first-page-clients-next",
+        btnPrev: ".first-page-clients-prev",
+        visible: 4,
+        auto: 5000,
+        speed: 2500
+    });
 
-$(function () {
+    $('.number-7').animateNumber({number: 7}, 3000);
+    $('.number-2000').animateNumber({number: 2000}, 3000);
+    $('.number-2457').animateNumber({number: 2457}, 3000);
+
+
+
     $('.second-page-order-form-date').datetimepicker(
         {pickTime: false, language: 'ru'}
     );
@@ -12,12 +22,21 @@ $(function () {
         {pickDate: false, language: 'ru'}
     );
     $('.second-page-address-subway').styler();
-});
-$('.second-page-delivery-btn, .second-page-pickup-btn').on('click', function(){
-    var sel = $(this).data('title');
-    var tog = $(this).data('toggle');
-    $('#'+tog).prop('value', sel);
 
-    $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
-    $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+    $('.second-page-delivery-btn, .second-page-pickup-btn').on('click', function(){
+        var sel = $(this).data('title');
+        var tog = $(this).data('toggle');
+        $('#'+tog).prop('value', sel);
+
+        $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
+        $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+    });
 });
+
+
+
+
+
+
+
+
