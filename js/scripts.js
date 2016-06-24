c4d71de43206be83502e1c869b154903502a7a56
Rat-Kat <kratush@gmail.com>
2016-06-24 10:58:27 +0300

    80% adaptiv carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
     $('.parallax').height($(window).height());
 
-    $(".first-page-clients-gallery").jCarouselLite({
-        btnNext: ".first-page-clients-next",
-        btnPrev: ".first-page-clients-prev",
-        visible: 4,
-        auto: 5000,
-        speed: 2500
-    });
+    var win_w = $(window).width();
+
+    if(win_w < 420) {
+        $(".first-page-clients-gallery").jCarouselLite({
+            btnNext: ".first-page-clients-next",
+            btnPrev: ".first-page-clients-prev",
+            visible: 1,
+            speed: 2500
+        });
+    } if(win_w >= 420 && win_w < 620) {
+        $(".first-page-clients-gallery").jCarouselLite({
+            btnNext: ".first-page-clients-next",
+            btnPrev: ".first-page-clients-prev",
+            visible: 2,
+            speed: 2500
+        });
+    }
+    if(win_w >= 620 && win_w < 1030){
+        $(".first-page-clients-gallery").jCarouselLite({
+            btnNext: ".first-page-clients-next",
+            btnPrev: ".first-page-clients-prev",
+            visible: 3,
+            speed: 2500
+        });
+    }
+    else {
+        $(".first-page-clients-gallery").jCarouselLite({
+            btnNext: ".first-page-clients-next",
+            btnPrev: ".first-page-clients-prev",
+            visible: 4,
+            speed: 2500
+        });
+    }
+
 
     $('.number-7').animateNumber({number: 7}, 3000);
     $('.number-2000').animateNumber({number: 2000}, 3000);
